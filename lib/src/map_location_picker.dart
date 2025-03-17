f2cdc5d1ba6293210f7cb9403c145e67a92b1d32
George Abd-Elmesseh <76843136+George-Abdelmessh@users.noreply.github.com>
2025-03-17 21:06:39 +0200

    Update map_location_picker.dart

--- lib/src/map_location_picker.dart
+++ lib/src/map_location_picker.dart
@@ -491,45 +491,50 @@ class _MapLocationPickerState extends State<MapLocationPicker> {
                     ),
                   ),
                 if (!widget.hideLocationButton)
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: FloatingActionButton(
-                      tooltip: widget.fabTooltip,
-                      backgroundColor: Theme.of(context).primaryColor,
-                      foregroundColor: Theme.of(context).colorScheme.onPrimary,
-                      onPressed: () async {
-                        // call parent method
-                        if (widget.getLocation != null) {
-                          widget.getLocation!.call();
-                        }
-
-                        if (widget.hasLocationPermission) {
-                          await Geolocator.requestPermission();
-                          Position position =
-                              await Geolocator.getCurrentPosition(
-                            desiredAccuracy: widget.desiredAccuracy,
-                          );
-                          LatLng latLng =
-                              LatLng(position.latitude, position.longitude);
-                          _initialPosition = latLng;
-                          final controller = await _controller.future;
-                          controller.animateCamera(
-                            CameraUpdate.newCameraPosition(
-                              cameraPosition(),
-                            ),
-                          );
-                          _decodeAddress(
-                            Location(
-                              lat: position.latitude,
-                              lng: position.longitude,
-                            ),
-                          );
-                          setState(() {});
-                        }
-                      },
-                      child: Icon(widget.fabIcon),
-                    ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.red,
                   ),
+                  // Padding(
+                  //   padding: const EdgeInsets.all(8.0),
+                  //   child: FloatingActionButton(
+                  //     tooltip: widget.fabTooltip,
+                  //     backgroundColor: Theme.of(context).primaryColor,
+                  //     foregroundColor: Theme.of(context).colorScheme.onPrimary,
+                  //     onPressed: () async {
+                  //       // call parent method
+                  //       if (widget.getLocation != null) {
+                  //         widget.getLocation!.call();
+                  //       }
+                  //
+                  //       if (widget.hasLocationPermission) {
+                  //         await Geolocator.requestPermission();
+                  //         Position position =
+                  //             await Geolocator.getCurrentPosition(
+                  //           desiredAccuracy: widget.desiredAccuracy,
+                  //         );
+                  //         LatLng latLng =
+                  //             LatLng(position.latitude, position.longitude);
+                  //         _initialPosition = latLng;
+                  //         final controller = await _controller.future;
+                  //         controller.animateCamera(
+                  //           CameraUpdate.newCameraPosition(
+                  //             cameraPosition(),
+                  //           ),
+                  //         );
+                  //         _decodeAddress(
+                  //           Location(
+                  //             lat: position.latitude,
+                  //             lng: position.longitude,
+                  //           ),
+                  //         );
+                  //         setState(() {});
+                  //       }
+                  //     },
+                  //     child: Icon(widget.fabIcon),
+                  //   ),
+                  // ),
                 if (!widget.hideBottomCard)
                   Card(
                     margin: widget.bottomCardMargin,
